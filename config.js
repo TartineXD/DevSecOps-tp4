892ab0d9468c19fb242ec66aa7b2925882dfba06
TartineXD <midnyght99@gmail.com>
2026-01-29 11:35:08 +0100

    Add Gitleaks workflow
    change v3

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuration de l’application
 const config = {
-  apiKey: "AKIAIOSFODNN7JBFKDLK",
-  secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+  apiKey: "AKIAIOS876FODNN7JBFKDLK",
+  secretKey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXa4667jd",
   dbPassword: "JNMONIVbh_123",
   githubToken: "ghp_xxxxfdjbhdgfjkgkxxxxxxxxxxxxxxxxxxxxxxxxx"
 };
